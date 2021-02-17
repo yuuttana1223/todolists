24ba599a1378e009ea834ac3fe41b2203e8013c8
EC2 Default User <ec2-user@ip-172-31-85-243.ec2.internal>
2021-02-17 05:26:42 +0000

    add delete

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 end
